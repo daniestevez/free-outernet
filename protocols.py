22f3e2a3574729868a6d26abf7b6a4c25df31a5c
George Hopkins <george-hopkins@null.net>
2017-01-07 21:30:57 +0100

    Remove trailing spaces

--- protocols.py
+++ protocols.py
@@ -1,20 +1,20 @@
 # Copyright 2016 Daniel Estevez <daniel@destevez.net>.
-# 
+#
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this software; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 """Outernet OP and LDP protocols"""
 
@@ -223,7 +223,7 @@ class LDPRouter:
         Create a new LDP router
         """
         self.__registrations = dict()
-        
+
     def route(self, packet):
         """
         Push an LDP packet into the router, calling the appropriate
